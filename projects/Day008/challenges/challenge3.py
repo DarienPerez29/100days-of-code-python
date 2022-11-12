fe94c6881641d3055abf4493b0005081d00ce971
Darien Pérez <dapec_290@hotmail.com>
2022-11-12 06:53:08 -0600

    D008: challenge3.1 (bug fixing)

--- projects/Day008/challenges/challenge3.py
+++ projects/Day008/challenges/challenge3.py
@@ -4,13 +4,14 @@ direction = input("Type 'enconde' to encrypt, type 'decode ' to decrypt: ")
 text = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
 
-def encrypt(text, shift):
+def encrypt(enc_text, enc_shift):
     secret = ""
 
-    for c in text:
-        secret += alphabet[(alphabet.index(c) + shift) % len(alphabet)]
+    for c in enc_text:
+        new_pos = (alphabet.index(c) + enc_shift) % len(alphabet)
+        secret += alphabet[new_pos]
     
     return secret
 
 if __name__ == "__main__":
-    print(encrypt(text = text, shift = shift))
+    print(encrypt(enc_text = text, enc_shift = shift))
