dd8e23d0b993c0c599f445cc79ba05c6b8aab770
Darien Pérez <dapec_290@hotmail.com>
2022-11-17 23:04:08 -0600

    D011: Player can get extra cards or stop

--- projects/Day011/blackjack.py
+++ projects/Day011/blackjack.py
@@ -2,11 +2,13 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] # Queen, King and Jack have a value of 10
 
+# Function to return list values in string
 def get_deck_str(deck, hide):
     if hide: 
         return str(deck[0]) + ", ■"
     return ', '.join(map(str, deck))
 
+# Function to return sum of list values
 def get_deck_sum(deck, hide):
     if hide:
         return deck[0]
@@ -14,13 +16,27 @@ def get_deck_sum(deck, hide):
 
 player_cards = []
 computer_cards = []
+hit = True
 
 for i in range(2):
     player_cards.append(random.choice(cards))
     computer_cards.append(random.choice(cards))
 
-print("=================================================")
-print(f"Your cards: {get_deck_str(player_cards, False)} \t|| Total: {get_deck_sum(player_cards, False)}")
-print("-------------------------------------------------")
-print(f"Computer cards: {get_deck_str(computer_cards, True)} \t|| Total: {get_deck_sum(computer_cards, True)}")
-print("=================================================")
+while hit:
+    print("=================================================")
+    print(f"Your cards: {get_deck_str(player_cards, False)} \t|| Total: {get_deck_sum(player_cards, False)}")
+    print("-------------------------------------------------")
+    print(f"Computer cards: {get_deck_str(computer_cards, True)} \t|| Total: {get_deck_sum(computer_cards, True)}")
+    print("=================================================\n")
+
+    while True:
+        action = str(input("Do you want to get another card? (y/n): ")).lower()
+
+        if action == "y" or action == "yes":
+            player_cards.append(random.choice(cards))
+            break
+        elif action == "n" or action == "no":
+            hit = False
+            break
+        else:
+            print("\nPlease, select a valid option...")
