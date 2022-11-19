b84eca95f879e5ebe8b23107967128a9622b614c
Darien Pérez <dapec_290@hotmail.com>
2022-11-19 10:08:49 -0600

    D012: Guessing number game runs

--- projects/Day012/.github/README.md
+++ projects/Day012/.github/README.md
@@ -1,3 +1,7 @@
 # Day 012 project - The Number Guessing Game
 
-*Description coming soon*
+This game consist on guessing a number between 1 and 100. It has two modes:
+1. **Easy mode:** You have 5 attempts to guess the number.
+2. **Hard mode:** You have 10 attempts to guess the number.
+
+<span style="color:gray">*You can ignore **practice** directory, just contains a few random programs to practice some basic concepts.*</span>
---
+++ projects/Day012/guessing_number.py
@@ -0,0 +1,52 @@
+import random
+
+number_to_guess = random.randint(1, 100) # Random number defined
+mode = ""
+lifes = 0
+guess = 0
+has_won = False
+
+# Welcome messages
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100\n")
+
+# Mode selection
+while True:
+    mode = input("Chose a difficulty (easy/hard): ").lower()
+    
+    if mode == "easy":
+        lifes = 10
+        break
+    elif mode == "hard":
+        lifes = 5
+        break
+    else:
+        print("\nPlease select a valid option...")
+
+# Guessing section
+while True:
+    if lifes >= 1: # Flow if player still has lifes
+        print(f"\nYou have {lifes} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        lifes -= 1
+
+        if guess < number_to_guess:
+            print(">>> Too low <<<")
+        elif guess > number_to_guess:
+            print(">>> Too high <<<")
+        else:
+            has_won = True
+            break
+        
+        if lifes > 0:
+            print("Guess again...", lifes)
+
+    else: # Run out of lifes
+        break
+
+
+# Determine if player won or lost
+if has_won:
+    print(f"\nYou got it! The answer was {number_to_guess}")
+else:
+    print(f"\nYou've run out of guesses, you lose. The number was {number_to_guess}")
