da514c0a9d5bda0b07f55cb72d0353caa2d0748e
Darien Pérez <dapec_290@hotmail.com>
2022-11-19 08:29:34 -0600

    D011: Replayable game feature added
    Now the player can decide if they want to play again or if they want to quit the game

--- projects/Day011/blackjack.py
+++ projects/Day011/blackjack.py
@@ -30,83 +30,98 @@ def print_scoreboard(is_player_turn):
     print(f"| Computer cards: {get_deck_str(computer_cards, is_player_turn)} \t|| Total: {computer_score}")
     print("=================================================\n")
 
-# Variables
-player_cards = []
-computer_cards = []
-player_score = 0
-computer_score = 0
+# Start game
+playing = True
+while playing:
+    # Variables
+    player_cards = []
+    computer_cards = []
+    player_score = 0
+    computer_score = 0
 
-player_turn = True
-winner = ""
+    player_turn = True
+    winner = ""
 
-# initial decks
-for i in range(2):
-    player_cards.append(random.choice(cards))
-    computer_cards.append(random.choice(cards))
-computer_score = get_deck_sum(computer_cards, player_turn) # Set computer score to display to the player
+    # initial decks
+    for i in range(2):
+        player_cards.append(random.choice(cards))
+        computer_cards.append(random.choice(cards))
+    computer_score = get_deck_sum(computer_cards, player_turn) # Set computer score to display to the player
 
-clear_screen()
+    clear_screen()
 
-# =====================================
-# Loop for the player to get more cards
-# =====================================
-while player_turn:
-    # The score of the desks is asked
-    player_score = get_deck_sum(player_cards, False)
-    
-    # If player score is perfect (21), they doesn't need to get more cards
-    if player_score == 21:
-        player_turn = False
-        break
-    # If player score is greater than 21, they automatically loses
-    elif player_score > 21:
-        winner = "computer"
-        player_turn = False
-        break
+    # =====================================
+    # Loop for the player to get more cards
+    # =====================================
+    while player_turn:
+        # The score of the desks is asked
+        player_score = get_deck_sum(player_cards, False)
+        
+        # If player score is perfect (21), they doesn't need to get more cards
+        if player_score == 21:
+            player_turn = False
+            break
+        # If player score is greater than 21, they automatically loses
+        elif player_score > 21:
+            winner = "computer"
+            player_turn = False
+            break
 
-    print_scoreboard(player_turn)
+        print_scoreboard(player_turn)
+
+        # If an invalid option is passed, the program asks again
+        while True:
+            action = str(input("Do you want to get another card? (y/n): ")).lower()
+            clear_screen()
+
+            if action == "y" or action == "yes":
+                player_cards.append(random.choice(cards))
+                break
+            elif action == "n" or action == "no":
+                player_turn = False
+                break
+            else:
+                clear_screen()
+                print_scoreboard(player_turn)
+                print("Please, select a valid option...")
+    # ===================================== player's turn ends
+
+    computer_score = get_deck_sum(computer_cards, player_turn) # Get real computer's score
+
+    # If computer already won, the game is over
+    if winner == "computer":
+        # End of game 1
+        print_scoreboard(player_turn)
+        print("\t       You went over...")
+        print(art.loser_icon)
+    else:    
+        # Loop for computer turn
+        while computer_score < player_score and computer_score < 17:
+            computer_cards.append(random.choice(cards))
+            computer_score = get_deck_sum(computer_cards, False)
+
+        # End of game 2
+        print_scoreboard(player_turn)
+        
+        if computer_score > 21:
+            print("\t    Opponent went over.")
+            print(art.winner_icon)
+        elif player_score > computer_score:
+            print(art.winner_icon)
+        elif player_score < computer_score:
+            print(art.loser_icon)
+        elif player_score == computer_score:
+            print("Draw!")
 
     # If an invalid option is passed, the program asks again
     while True:
-        action = str(input("Do you want to get another card? (y/n): ")).lower()
-        clear_screen()
+        play_again = str(input("Do you want to play again? (y/n): ")).lower()
 
-        if action == "y" or action == "yes":
-            player_cards.append(random.choice(cards))
+        if play_again == "y" or play_again == "yes":
             break
-        elif action == "n" or action == "no":
-            player_turn = False
+        elif play_again == "n" or play_again == "no":
+            print("\nBye :D")
+            playing = False
             break
         else:
-            clear_screen()
-            print_scoreboard(player_turn)
-            print("Please, select a valid option...")
-# ===================================== player's turn ends
-
-computer_score = get_deck_sum(computer_cards, player_turn) # Get real computer's score
-
-# If computer already won, the game is over
-if winner == "computer":
-    # End of game 1
-    print_scoreboard(player_turn)
-    print("\t       You went over...")
-    print(art.loser_icon)
-else:    
-    # Loop for computer turn
-    while computer_score < player_score and computer_score < 17:
-        computer_cards.append(random.choice(cards))
-        computer_score = get_deck_sum(computer_cards, False)
-
-    # End of game 2
-    print_scoreboard(player_turn)
-    
-    if computer_score > 21:
-        print("\t    Opponent went over.")
-        print(art.winner_icon)
-    elif player_score > computer_score:
-        print(art.winner_icon)
-    elif player_score < computer_score:
-        print(art.loser_icon)
-    elif player_score == computer_score:
-        print("Draw!")
-        
+            print("\nPlease, select a valid option...")
