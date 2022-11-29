ac0f119aa4706471f0813b89b75ec2243a1fb025
Darien Pérez <dapec_290@hotmail.com>
2022-11-29 17:23:35 -0600

    D014: Clearing screen terminal and printing improved

--- projects/Day014/higher_lower_game.py
+++ projects/Day014/higher_lower_game.py
@@ -1,6 +1,11 @@
+import os
 import random
 import modules.art as art
 import modules.data as gd
+
+# Function to clear screen in windows or linux
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 # Function to print data of an option in a formatted way
 def print_formatted_option(data, show_followers):
@@ -15,8 +20,11 @@ def print_formatted_option(data, show_followers):
 # Function to print both options, a and b
 def print_options(op_a, op_b):
     print_formatted_option(op_a, True)
-    print("====== vs =======")
+    print("\t====== vs =======")
     print_formatted_option(op_b, False)
+
+# Intro
+clear_screen()
 
 print(art.logo)
 
@@ -27,24 +35,35 @@ print('''
 ''')
 input("         Press a key to continue...")
 
+clear_screen()
+
 # Init variables with default values
 option_a = random.choice(gd.data)
 option_b = {}
 score_a = 0
 score_b = 0
-final_score = 0
+score = 0
 
 # Loop for playing until lose
 has_lost = False
 while not has_lost:
+
     option_b = random.choice(gd.data)
     score_a = option_a['follower_count']
     score_b = option_b['follower_count']
 
-    print_options(option_a, option_b)
-
-    # Loop for requset for a valid response
+    # Loop for requset for a valid response (higher/lower)
     while True:
+        print(art.logo)
+
+        print(f"""
+                ==================
+                | Score: {score}\t|
+                ==================""")
+
+
+        print_options(option_a, option_b)
+
         print(f">> Does \"{option_b['name']}\" have higher ", end="")
         print(f"or lower followers than \"{option_a['name']}\"?")
 
@@ -52,22 +71,28 @@ while not has_lost:
 
         if answer != "higher" and answer != "lower":
             print("\nPlease select a valid option...")
+            clear_screen()
         else:
             break
 
+    
     # Check answer of player
     if (answer == "higher" and score_b >= score_a # Checks for higher
             or answer == "lower" and score_b <= score_a): # Checks for lower
-        print("\n=== Correct ===")
-        final_score += 1
+        print("\n[CORRECT]: ", end = "")
+        score += 1
         option_a = option_b
     else:
-        print("\n=== You lose ===")
+        print("\n[YOU LOSE]: ", end = "")
         has_lost = True
-    print(f"{option_b['name']} has {option_b['follower_count']} million followers")
+    print(f"{option_b['name']} has {option_b['follower_count']} million followers\n")
+    
+    input("Press a key to continue...")
+    clear_screen()
 
+print(art.logo)
 
 print(f"""
-========================================
-\tYour final score is: {final_score}  
-========================================""")
+     ========================================
+             Your final score is: {score}  
+     ========================================""")
