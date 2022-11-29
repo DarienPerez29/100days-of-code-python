d031eecd2b95c696d8182970b1f7a8f014a3edb5
Darien Pérez <dapec_290@hotmail.com>
2022-11-29 17:45:57 -0600

    D014: Replay function implemented

--- projects/Day014/higher_lower_game.py
+++ projects/Day014/higher_lower_game.py
@@ -23,81 +23,96 @@ def print_options(op_a, op_b):
     print("\t====== vs =======")
     print_formatted_option(op_b, False)
 
-# Intro
-clear_screen()
+# Loop for replay game
+playing = True
+while playing:
+    # Intro
+    clear_screen()
 
-print(art.logo)
+    print(art.logo)
 
-print('''
-   ============================================
-   >>> Who has more followers on Instagram? <<<
-   ============================================
-''')
-input("         Press a key to continue...")
+    print('''
+    ============================================
+    >>> Who has more followers on Instagram? <<<
+    ============================================
+    ''')
+    input("         Press a key to continue...")
 
-clear_screen()
+    clear_screen()
 
-# Init variables with default values
-option_a = random.choice(gd.data)
-option_b = {}
-score_a = 0
-score_b = 0
-score = 0
+    # Init variables with default values
+    option_a = random.choice(gd.data)
+    option_b = {}
+    score_a = 0
+    score_b = 0
+    score = 0
 
-# Loop for playing until lose
-has_lost = False
-while not has_lost:
+    # Loop for playing until lose
+    has_lost = False
+    while not has_lost:
 
-    # Loop to select a different account
-    while True:
-        option_b = random.choice(gd.data)
-        if option_b['name'] != option_a['name']:
-            break
-        
-    score_a = option_a['follower_count']
-    score_b = option_b['follower_count']
+        # Loop to select a different account
+        while True:
+            option_b = random.choice(gd.data)
+            if option_b['name'] != option_a['name']:
+                break
+            
+        score_a = option_a['follower_count']
+        score_b = option_b['follower_count']
 
-    # Loop for requset for a valid response (higher/lower)
-    while True:
-        print(art.logo)
+        # Loop for requset for a valid response (higher/lower)
+        while True:
+            print(art.logo)
 
-        print(f"""
+            print(f"""
                 ==================
                 | Score: {score}\t|
                 ==================""")
 
 
-        print_options(option_a, option_b)
+            print_options(option_a, option_b)
 
-        print(f">> Does \"{option_b['name']}\" have higher ", end="")
-        print(f"or lower followers than \"{option_a['name']}\"?")
+            print(f">> Does \"{option_b['name']}\" have higher ", end="")
+            print(f"or lower followers than \"{option_a['name']}\"?")
 
-        answer = input("   (Type higher or lower): ")
+            answer = input("   (Type higher or lower): ")
 
-        if answer != "higher" and answer != "lower":
-            print("\nPlease select a valid option...")
-            clear_screen()
+            if answer != "higher" and answer != "lower":
+                clear_screen()
+                print("\n>>>>>>>>>>>>>>>> Please select a valid option...")
+            else:
+                break
+
+        # Check answer of player
+        if (answer == "higher" and score_b >= score_a # Checks for higher
+                or answer == "lower" and score_b <= score_a): # Checks for lower
+            print("\n[CORRECT]: ", end = "")
+            score += 1
+            option_a = option_b
         else:
+            print("\n[YOU LOSE]: ", end = "")
+            has_lost = True
+        print(f"{option_b['name']} has {option_b['follower_count']} million followers\n")
+        
+        input("Press a key to continue...")
+        clear_screen()
+
+    print(art.logo)
+
+    print(f"""
+    ========================================
+            Your final score is: {score}  
+    ========================================\n""")
+
+    while True:
+        option = input("Do you want to play again? (y/n)")
+
+        if option == "n" or option == "no":
+            clear_screen()
+            print("Bye...")
+            playing = False
             break
-
-    
-    # Check answer of player
-    if (answer == "higher" and score_b >= score_a # Checks for higher
-            or answer == "lower" and score_b <= score_a): # Checks for lower
-        print("\n[CORRECT]: ", end = "")
-        score += 1
-        option_a = option_b
-    else:
-        print("\n[YOU LOSE]: ", end = "")
-        has_lost = True
-    print(f"{option_b['name']} has {option_b['follower_count']} million followers\n")
-    
-    input("Press a key to continue...")
-    clear_screen()
-
-print(art.logo)
-
-print(f"""
-     ========================================
-             Your final score is: {score}  
-     ========================================""")
+        elif option == "y" or option == "yes":
+            break
+        else:
+            print("\nPlease select a valid option...")
