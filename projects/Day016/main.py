234a9760ba3178f3d49ae1bed2628b60cab30322
Darien Pérez <dapec_290@hotmail.com>
2022-12-01 21:58:41 -0600

    D016: Turn off function implemented

--- projects/Day016/main.py
+++ projects/Day016/main.py
@@ -1,3 +1,13 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from modules.menu import Menu, MenuItem
+from modules.coffee_maker import CoffeeMaker
+from modules.money_machine import MoneyMachine
+
+machine_running = True
+
+while machine_running:
+    command = input(">> What would you like? (espresso/latte/cappuccino):")
+    if command == "off":
+        print("\nTurning machine off...")
+        machine_running = False
+    else:
+        print("\nPlease insert a valid option...")
