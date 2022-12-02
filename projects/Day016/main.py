4bca47b6236d98b3d133b6ba592d7d99614527a7
Darien Pérez <dapec_290@hotmail.com>
2022-12-01 22:02:44 -0600

    D016: Report function implemented

--- projects/Day016/main.py
+++ projects/Day016/main.py
@@ -2,12 +2,18 @@ from modules.menu import Menu, MenuItem
 from modules.coffee_maker import CoffeeMaker
 from modules.money_machine import MoneyMachine
 
+my_menu = Menu()
+my_coffee_mkr = CoffeeMaker()
+my_money_machine = MoneyMachine()
+
 machine_running = True
 
 while machine_running:
-    command = input(">> What would you like? (espresso/latte/cappuccino):")
+    command = input(">> What would you like? (espresso/latte/cappuccino): ")
     if command == "off":
         print("\nTurning machine off...")
         machine_running = False
+    elif command == "report":
+        my_coffee_mkr.report()
     else:
         print("\nPlease insert a valid option...")
