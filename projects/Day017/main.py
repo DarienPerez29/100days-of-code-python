d4c493fce069265a59e07e5aba607cc522026f92
Darien Pérez <dapec_290@hotmail.com>
2022-12-05 20:34:10 -0600

    D017: Changing dataset of quizzes

--- projects/Day017/main.py
+++ projects/Day017/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    curr_text = question['text']
-    curr_answ = question['answer']
+    curr_text = question['question']
+    curr_answ = question['correct_answer']
 
     curr_question = Question(curr_text, curr_answ)
     question_bank.append(curr_question)
