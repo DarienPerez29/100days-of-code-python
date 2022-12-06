0c6a518383c3e9f5ad2e2c27dd2db2de5c267370
Darien Pérez <dapec_290@hotmail.com>
2022-12-05 18:27:55 -0600

    D017: practice2

--- projects/Day017/practice/classes1_main.py
+++ projects/Day017/practice/classes1_main.py
@@ -3,10 +3,22 @@ class User:
         self.id_user = id_user
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User(1, "Usuario1")
 user2 = User(2, "Usuario2")
 
-print(user1.username)
-print(user2.username)
+user1.follow(user2)
+
+print("USER 1")
+print(f"Followers: {user1.followers}")
+print(f"Following: {user1.following}")
+print()
+print("USER 2")
+print(f"Followers: {user2.followers}")
+print(f"Following: {user2.following}")
