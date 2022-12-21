087cfcda68a6e970da678943ccbd6a4d17574b28
Darien Pérez <dapec_290@hotmail.com>
2022-12-20 23:30:29 -0600

    D019: Etch a sketch pen down/up function

--- projects/Day019/etch_a_sketch.py
+++ projects/Day019/etch_a_sketch.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 
 t = Turtle()
+pen_down = True
 s = Screen()
 s.listen()
 
@@ -25,10 +26,21 @@ def turn_right():
     t.right(10)
 
 
+def alter_pen():
+    global pen_down
+    if pen_down:
+        t.penup()
+        pen_down = False
+    else:
+        t.pendown()
+        pen_down = True
+
+
 s.onkey(clear, "c")
 s.onkeypress(move_forward, "w")
 s.onkeypress(move_backward, "s")
 s.onkeypress(turn_left, "a")
 s.onkeypress(turn_right, "d")
+s.onkeypress(alter_pen, "p")
 
 s.exitonclick()
